96f70f9299a76d1573ef76c0afa404f09b5fa970
Conor Proffitt <conor.proffitt@gmail.com>
2023-10-18 20:01:17 -0400

    added elephant

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const contents = document.getElementById('contents')
-contents.parentNode.removeChild(contents)
+//const contents = document.getElementById('contents')
+////contents.parentNode.removeChild(contents)
+document.addEventListener('DOMContentLoaded', () => {
+const images = document.querySelectorAll('img')
 
-setInterval(alert('asdfasdf'), 1000)
+for(let ele of array){
+
+    ele.src = "https://pics.craiyon.com/2023-07-19/ce7d04fee3924afba897a478efbd5923.webp"
+
+}
+setInterval(alert('ANIMAL'), 1000)
+
+})
